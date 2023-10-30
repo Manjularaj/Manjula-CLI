84c79fea826b6f9c454df02e430efb0b673456c0
Manjularaj <manjularajanap@gmail.com>
2023-10-30 14:29:47 -0700

    intial commit

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Manjula_CLI;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Manjula_CLI;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
